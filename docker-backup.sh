f76f7c49c8aada35735340d6bff901ce43e1b06b
napnap75 <thomas@nappez.com>
2017-06-25 23:29:05 +0200

    Update docker-backup.sh

--- docker-backup.sh
+++ docker-backup.sh
@@ -12,27 +12,7 @@ sleep_until() {
     sleep $slp
 }
 
-# Check the connection to the host and ensure the directory with the name of the node exists and is writeable
-# By the way, this will also load the known_host file with the host key if not already set
-function check_connection {
-	touch test_file.in
-	sftp -oStrictHostKeyChecking=no -oHostKeyAlgorithms=ssh-rsa -q -b - -i $SFTP_KEY -P $SFTP_PORT $SFTP_USER@$SFTP_HOST <<EOF
-put test_file.in $1/test_file.in
-rename $1/test_file.in $1/test_file.out
-get $1/test_file.out
-rm $1/test_file.out
-exit
-EOF
-	if [ -f "test_file.out" ] ; then
-		rm test_file.in test_file.out
-		return 0
-	else
-		rm test_file.in
-		return 1
-	fi
-}
-
-# Backup one directory using duplicity
+# Backup one directory using restic
 function backup_dir {
 	# Check if the dir to backup is mounted as a subdirectory of /root inside this container
 	if [ -d "/root_fs$2" ] ; then
@@ -88,23 +68,17 @@ echo "IdentityFile $SFTP_KEY" >> /root/.ssh/config
 
 # First check the connection
 echo "[INFO] Trying to connect to host $SFTP_HOST"
-check_connection $NODE_NAME
-if [ $? == 0 ] ; then
-	if [ "$1" == "run-once" ] ; then
-		# Run only once, mainly for tests purpose
-		start_time=$(($RANDOM % 7)):$(($RANDOM % 60))
-		echo "[INFO] Backup would have started at $start_time every day"
-		run_backup $NODE_NAME
-	else
-		# Run everyday at $start_time
-		start_time=$(($RANDOM % 7)):$(($RANDOM % 60))
-		echo "[INFO] Backup will start at $start_time every day"
-		while true ; do
-			sleep_until $start_time
-			run_backup $NODE_NAME
-		done
-	fi
+if [ "$1" == "run-once" ] ; then
+	# Run only once, mainly for tests purpose
+	start_time=$(($RANDOM % 7)):$(($RANDOM % 60))
+	echo "[INFO] Backup would have started at $start_time every day"
+	run_backup $NODE_NAME
 else
-	echo "[ERROR] Unable to connect to host $SFTP_HOST"
-	exit 1
+	# Run everyday at $start_time
+	start_time=$(($RANDOM % 7)):$(($RANDOM % 60))
+	echo "[INFO] Backup will start at $start_time every day"
+	while true ; do
+		sleep_until $start_time
+		run_backup $NODE_NAME
+	done
 fi
