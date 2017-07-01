543fb8b7c3b6a250d7b80f4e095a0f60ef2aebcd
Thomas <thomas@nappez.com>
2017-07-01 23:01:30 +0200

    Correctly handle the hostname in case of a Swarm

--- docker-backup.sh
+++ docker-backup.sh
@@ -54,7 +54,8 @@ function check_connection {
 function backup_dir {
 	# Check if the dir to backup is mounted as a subdirectory of /root inside this container
 	if [ -d "/root_fs$1" ] ; then
-		restic backup /root_fs$1
+		echo "[DEBUG] restic --hostname $2 backup /root_fs$1"
+		restic --hostname $2 backup /root_fs$1
 	else
 		echo "[ERROR] Directory" $1 "not found. Have you mounted the root fs from your host with the following option : '-v /:/root_fs:ro' ?"
 	fi
@@ -75,7 +76,7 @@ function run_backup {
 		if $(echo $container | jq ".Labels | has(\"napnap75.backup.dirs\")") ; then
 			for dir_name in $(echo $container | jq -r ".Labels | .[\"napnap75.backup.dirs\"]") ; do
 				echo "[INFO] Backing up dir" $dir_name "for container" $container_name
-				backup_dir $dir_name
+				backup_dir $dir_name $1
 			done
 		fi
 
@@ -85,7 +86,7 @@ function run_backup {
 				if [ $namespace != "null" ] ; then volume_name="${namespace}_${volume_name}" ; fi
 				volume_mount=$(echo $container | jq -r ".Mounts[] | select(.Name==\"$volume_name\") | .Source")
 				echo "[INFO] Backing up volume" $volume_name "with mount" $volume_mount "for container" $container_name
-				backup_dir $volume_mount
+				backup_dir $volume_mount $1
 			done
 		fi
 	done
@@ -95,7 +96,7 @@ function run_backup {
 NODE_NAME=$(curl -s --unix-socket /var/run/docker.sock http:/v1.26/info | jq -r ".Name")
 if [[ "$NODE_NAME" != "" ]] ; then
 	echo "[INFO] Swarm mode detected, using node name $NODE_NAME as hostname"
-	HOSTNAME="$NODE_NAME"
+	HOSTNAME=$NODE_NAME
 fi
 
 # When used with SFTP
@@ -120,14 +121,14 @@ if [ $? == 0 ] ; then
 		# Run only once, mainly for tests purpose
 		start_time=$(($RANDOM % 7)):$(($RANDOM % 60))
 		echo "[INFO] Backup would have started at $start_time every day"
-		run_backup
+		run_backup $HOSTNAME
 	else
 		# Run everyday at $start_time
 		start_time=$(($RANDOM % 7)):$(($RANDOM % 60))
 		echo "[INFO] Backup will start at $start_time every day"
 		while true ; do
 			sleep_until $start_time
-			run_backup
+			run_backup $HOSTNAME
 		done
 	fi
 else
